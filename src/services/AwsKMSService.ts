d80ca7091b6e6b80aa5b3b5997e0595c0d02185e
JasonLi <jli@mumba.cloud>
2020-10-11 19:36:38 +1100

    implemented constructor unit test

--- src/services/AwsKMSService.ts
+++ src/services/AwsKMSService.ts
@@ -1,6 +1,7 @@
 import { KMS } from 'aws-sdk';
-import { DecryptRequest, EncryptRequest } from 'aws-sdk/clients/kms';
+import { EncryptRequest } from 'aws-sdk/clients/kms';
 import { BaseFileService, FileOptions } from './BaseFileService';
+import { E_KMS_KEYID_UNDEFINED } from '../messages';
 
 export interface KMSFileOptions extends FileOptions {
   KeyId: string;
@@ -10,6 +11,11 @@ export class AwsKMSService extends BaseFileService<KMSFileOptions>{
 
   constructor(options: KMSFileOptions, private kms: KMS) {
     super(options);
+
+    // Validate if KeyId value available
+    if (!options.KeyId) {
+      throw new Error(E_KMS_KEYID_UNDEFINED);
+    }
   }
 
   /**
@@ -19,7 +25,15 @@ export class AwsKMSService extends BaseFileService<KMSFileOptions>{
    * @returns @returns {Promise<string>}
    */
   public async encrypt(rawData: Buffer): Promise<string> {
-    return '';
+    const params = {
+      KeyId: this.getOptions().KeyId,
+      Plaintext: rawData
+    } as EncryptRequest;
+
+    const { CiphertextBlob } = await this.kms.encrypt(params).promise();
+
+    // store encrypted data as base64 encoded string
+    return CiphertextBlob.toString('base64');
   }
 
   /**
--- test/src/services/AwsKMSService.unit.test.ts
+++ test/src/services/AwsKMSService.unit.test.ts
@@ -4,14 +4,15 @@ import * as assert from 'assert';
 import { AwsKMSService } from '../../../src/services/AwsKMSService';
 import { E_KMS_KEYID_UNDEFINED } from '../../../src/messages';
 
-describe('AwsKMSService unit test', () => {
+describe.only('AwsKMSService unit test', () => {
   let options: KMSFileOptions;
   let kms: KMS;
   let instance: AwsKMSService;
 
   beforeEach(() => {
     options = {
-      KeyId: 'key-id'
+      KeyId: 'key-id',
+      localDir: __dirname + '/tmp'
     } as KMSFileOptions;
     kms = {} as KMS;
     instance = new AwsKMSService(options, kms);
@@ -20,19 +21,19 @@ describe('AwsKMSService unit test', () => {
 
   it('should throw if `options.KeyId` is missing in the constructor', () => {
     assert.throws(() => {
-      instance = new AwsKMSService({ localDir: __dirname + '/tmp' } as KMSFileOptions, kms);
+      instance = new AwsKMSService({ KeyId: '', localDir: __dirname + '/tmp' } as KMSFileOptions, kms);
     }, new RegExp(E_KMS_KEYID_UNDEFINED));
   });
 
   it('should encrypt raw data', async () => {
     const expect: string = '';
-    const data: Buffer = new Buffer('test-data', 'utf-8');
+    const data: Buffer = Buffer.from('test-data', 'utf-8');
 
     const result = await instance.encrypt(data);
     assert.strictEqual(result, expect);
   });
 
-  it('should decrypt encrypted data to raw data', async () => {
+  it.skip('should decrypt encrypted data to raw data', async () => {
     const expect: string = '';
     const data: string = 'test-data';
 
